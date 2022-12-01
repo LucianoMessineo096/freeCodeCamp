83e6c5ea478d06174e30f92a7df3fd4684b02ec3
LucianoMessineo096 <89654809+LucianoMessineo096@users.noreply.github.com>
2022-12-01 17:14:53 +0100

    Update queries.sh

--- WorldCup/queries.sh
+++ WorldCup/queries.sh
@@ -4,20 +4,26 @@ PSQL="psql --username=freecodecamp --dbname=worldcup --no-align --tuples-only -c
 
 # Do not change code above this line. Use the PSQL variable above to query your database.
 
+#! /bin/bash
+
+PSQL="psql --username=freecodecamp --dbname=worldcup --no-align --tuples-only -c"
+
+# Do not change code above this line. Use the PSQL variable above to query your database.
+
 echo -e "\nTotal number of goals in all games from winning teams:"
 echo "$($PSQL "SELECT SUM(winner_goals) FROM games")"
 
 echo -e "\nTotal number of goals in all games from both teams combined:"
-echo "$($PSQL "SELECT SUM(winner_goals) FROM games")"
-
-echo -e "\nAverage number of goals in all games from the winning teams:"
 echo "$($PSQL "SELECT SUM(winner_goals)+SUM(opponent_goals) FROM games")"
 
-echo -e "\nAverage number of goals in all games from the winning teams rounded to two decimal places:"
+echo -e "\nAverage number of goals in all games from the winning teams:"
 echo "$($PSQL "SELECT AVG(winner_goals) FROM games")"
 
+echo -e "\nAverage number of goals in all games from the winning teams rounded to two decimal places:"
+echo "$($PSQL "SELECT ROUND(AVG(winner_goals),2) FROM games")"
+
 echo -e "\nAverage number of goals in all games from both teams:"
-echo "$($PSQL "SELECT AVG(winner_goals)+AVG(opponent_goals) FROM games")"
+echo "$($PSQL "SELECT ROUND(AVG(winner_goals)+AVG(opponent_goals),16) FROM games")"
 
 echo -e "\nMost goals scored in a single game by one team:"
 echo "$($PSQL "SELECT MAX(winner_goals) FROM games")"
@@ -38,5 +44,4 @@ echo -e "\nYear and team name of all the champions:"
 echo "$($PSQL "SELECT year,name FROM teams INNER JOIN games ON teams.team_id=games.winner_id WHERE round='Final' AND winner_goals>opponent_goals GROUP BY year,name")"
 
 echo -e "\nList of teams that start with 'Co':"
-echo "$($PSQL "SELECT name FROM teams INNER JOIN games ON teams.team_id=games.winner_id WHERE name LIKE 'Co%'")"
-
+echo "$($PSQL "SELECT name FROM teams WHERE name LIKE 'Co%'")"
