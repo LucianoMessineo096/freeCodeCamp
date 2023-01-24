4df4a8a5f3c94e23605f8e21465d42f0e13e4038
LucianoMessineo096 <89654809+LucianoMessineo096@users.noreply.github.com>
2023-01-24 10:58:23 +0100

    Update UrlServices.js

--- Back End Development and API's/Url Shortener Microservice/model/UrlServices.js
+++ Back End Development and API's/Url Shortener Microservice/model/UrlServices.js
@@ -1,1 +1,38 @@
+const shortId = require('shortId');
+const mongoose = require('mongoose')
+const {Schema} =  mongoose;
+const UrlModel = require('./UrlSchema');
 
+async function findShortUrl(shortUrl){
+
+    return await UrlModel.findOne({short_url:shortUrl}).exec();
+
+}
+
+async function saveUrl(url,req,res){
+
+    try{
+
+        let urlParams = await UrlModel.findOne({long_url:url}).exec();
+
+        if(!urlParams){
+
+            let short_url = shortId.generate()
+            const newUrl = new UrlModel({long_url:url,short_url:short_url})
+            await newUrl.save();
+
+            res.json({original_url:url,short_url:short_url});
+
+        }else{
+
+            res.json({original_url:urlParams.long_url,short_url:urlParams.short_url});
+            
+        }
+
+    }catch(err){
+
+        res.json({error:"url not saved"});
+    }
+}
+
+module.exports = {saveUrl,findShortUrl}
