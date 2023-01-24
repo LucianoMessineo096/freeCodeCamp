598af3cefb1c24d2ce85b0265acc5b528fc37840
LucianoMessineo096 <89654809+LucianoMessineo096@users.noreply.github.com>
2023-01-24 10:58:07 +0100

    Update UrlSchema.js

--- Back End Development and API's/Url Shortener Microservice/model/UrlSchema.js
+++ Back End Development and API's/Url Shortener Microservice/model/UrlSchema.js
@@ -1,1 +1,14 @@
+const mongoose = require('mongoose');
+const {Schema} =  mongoose;
 
+const urlSchema = new Schema({
+
+    long_url: { type: String, required: true },
+    short_url: {type: String, unique:true}
+
+});
+
+var Url = mongoose.model('url', urlSchema);
+
+
+module.exports = Url;
